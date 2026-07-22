de42b584c151ed327eba74ee92b77a66e8e82c59
Amanul Riad <amanulriad32@gmail.com>
2026-07-22 18:47:31 +0000

    Complete Day-1 JS exercises

---
+++ Day-Ex-1/datatypes.js
@@ -0,0 +1,5 @@
+console.log(typeof "Aman");
+console.log(typeof true);
+console.log(typeof 25);
+console.log(typeof undefined);
+console.log(typeof null);
---
+++ Day-Ex-1/variables.js
@@ -0,0 +1,32 @@
+let name = "Aman";
+let isStudent = true;
+let city;
+let data = null;
+
+let firstName;
+let lastName;
+let age;
+let country;
+
+
+let firstName = "Aman";
+let lastName = "Riad";
+let age = 30;
+let country = "Bangladesh";
+
+
+let firstName = "Amanul";
+let lastName = "Riad";
+let maritalStatus = "Single"
+let age = 30;
+let country = "Bangladesh";
+
+
+let firstName = "Amanul", lastName = "Riad", maritalStatus = "Single", age = 30, country = "Bangladesh";
+
+
+let myAge = 30;
+let yourAge = 25;
+
+console.log(`I am ${myAge} years old.`);
+console.log(`You are ${yourAge} years old.`);
